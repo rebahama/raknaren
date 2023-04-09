b6a11fd6028e3895be683532170e8e228283ab28
rebahama <wiri@hotmail.se>
2023-04-09 20:37:22 +0000

    add calculator and post modal

---
+++ posts/models.py
@@ -0,0 +1,21 @@
+
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
+from django.contrib.auth.models import User
+from .countmodel import Calculator
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False)
+    content = models.CharField(max_length=2000, blank=False)
+    calculate = models.ForeignKey(Calculator, on_delete=models.CASCADE, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.owner} {self.title}'
