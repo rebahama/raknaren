cab13702305f8c38cd597a1b7d39f24a5003ee4f
rebahama <wiri@hotmail.se>
2023-04-10 13:56:34 +0000

    add url auth

--- raknaren_drf/settings.py
+++ raknaren_drf/settings.py
@@ -58,7 +58,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),'localhost']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost']
 
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
