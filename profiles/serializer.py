cfa15699cfc98c33a36f0e90530625d8581f5486
rebahama <wiri@hotmail.se>
2023-04-11 15:44:53 +0000

    add time and date

--- profiles/serializer.py
+++ profiles/serializer.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers, generics
 from . models import Profile
 
@@ -5,11 +6,19 @@ from . models import Profile
 class ProfileSeralizer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         """ How to check for profile ownership"""
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Profile
